9c4b268c9987397224b8ddbad3fd3e1f962cbdba
Lina Do <l.do5147@gmail.com>
2021-01-20 18:31:33 -0800

    added create action to post a new video to library

--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -66,7 +66,26 @@ class VideosController < ApplicationController
     # end
   end
 
+  def create
+    @video = Video.new(video_params)
+
+    if @video.save
+      render(
+          status: :ok,
+          json: @video.as_json(
+              only: [:title, :overview, :release_date, :external_id, :image_url],
+          )
+      )
+    else
+       render json: {ok: false, cause: "validation errors", errors: @video.errors}, status: :bad_request
+    end
+  end
+
   private
+
+  def video_params
+    return params.permit(:external_id, :overview, :image_url, :release_date, :title )
+  end
 
   def require_video
     @video = Video.find_by(title: params[:title])
